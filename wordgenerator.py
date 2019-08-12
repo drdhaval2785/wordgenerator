da5e17aed5de6e7ae9fb4f899ffe601b7107146f
drdhaval2785 <drdhaval2785@gmail.com>
2019-08-12 12:48:41 +0530

    Gujarati words added

--- wordgenerator.py
+++ wordgenerator.py
@@ -6,8 +6,11 @@ from indic_transliteration import sanscript
 
 def createWordList(headwordfile):
 	with open(headwordfile, 'r') as fin:
-		result = [x.split(':')[0] for x in fin]
+		result = [x.split(':')[0].rstrip() for x in fin]
 	return result
+
+def guj_to_slp1(lst):
+	return [sanscript.transliterate(x, 'gujarati', 'slp1') for x in lst]
 
 def pruneByPatterns(hwList, requiredPatterns, prohibitedPatterns):
 	result = []
@@ -20,10 +23,17 @@ if __name__ == "__main__":
 	requiredPatterns = sys.argv[1]
 	prohibitedPatterns = sys.argv[2]
 	number = int(sys.argv[3])
-	hwList = createWordList('sanhw1.txt')
+	length = int(sys.argv[4])
+	#hwList = createWordList('sanhw1.txt')
+	hwList = createWordList('gujaratiwords.txt')
+	hwList = guj_to_slp1(hwList)
 	prunedList = pruneByPatterns(hwList, requiredPatterns, prohibitedPatterns)
-	result = random.sample(prunedList, number)
-	result1 = [sanscript.transliterate(x, 'slp1', 'devanagari') for x in result]
-	for res in result1:
+	result = [x for x in prunedList if len(x) <= length]
+	result1 = [sanscript.transliterate(x, 'slp1', 'gujarati') for x in result]
+	if len(result1) > number:
+		result2 = random.sample(result1, number)
+	else:
+		result2 = result1
+	for res in result2:
 		print(res)
 
