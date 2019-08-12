89220cfa13cdf61a2bc4a7980dd2a35e1b18888b
drdhaval2785 <drdhaval2785@gmail.com>
2019-08-12 12:29:52 +0530

    devanagari output randomly picked

--- wordgenerator.py
+++ wordgenerator.py
@@ -1,6 +1,7 @@
 import sys
 import codecs
 import re
+import random
 from indic_transliteration import sanscript
 
 def createWordList(headwordfile):
@@ -9,13 +10,20 @@ def createWordList(headwordfile):
 	return result
 
 def pruneByPatterns(hwList, requiredPatterns, prohibitedPatterns):
+	result = []
 	for hw in hwList:
 		if re.search(requiredPatterns, hw) and not re.search(prohibitedPatterns, hw):
-			print(hw)
+			result.append(hw)
+	return result
 
 if __name__ == "__main__":
 	requiredPatterns = sys.argv[1]
 	prohibitedPatterns = sys.argv[2]
+	number = int(sys.argv[3])
 	hwList = createWordList('sanhw1.txt')
-	pruneByPatterns(hwList, requiredPatterns, prohibitedPatterns)
+	prunedList = pruneByPatterns(hwList, requiredPatterns, prohibitedPatterns)
+	result = random.sample(prunedList, number)
+	result1 = [sanscript.transliterate(x, 'slp1', 'devanagari') for x in result]
+	for res in result1:
+		print(res)
 
